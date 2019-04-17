12988480840d2361a01c2cb60587961960ecb0c8
Chunpeng Huo <huocp@me.com>
2019-04-18 09:55:42 +1000

    refactor: simplify repo path, cleanup log

--- test/skeleton-dir.spec.js
+++ test/skeleton-dir.spec.js
@@ -3,7 +3,6 @@ import skeletonDir from '../lib/skeleton-dir';
 import mockfs from 'mock-fs';
 import path from 'path';
 import fs from 'fs';
-import rmdir from 'rimraf';
 
 test.afterEach(() => {
   mockfs.restore();
@@ -48,41 +47,3 @@ test.serial('skeletonDir returns tmp folder untar git repo, and caches it', asyn
   t.truthy(fs.readFileSync(path.join(dir2, 'README.md'), 'utf8').includes('debug repo for npm'));
 });
 
-test.serial('skeletonDir complains about unknown repo type', async t => {
-  function resolveRepo() {
-    return {
-      type: 'subversion',
-      repo: 'lorem'
-    };
-  }
-
-  await t.throwsAsync(async() => skeletonDir('something', {
-    _resolve: resolveRepo
-  }));
-});
-
-test.serial('skeletonDir returns tmp folder checked out git repo, and caches it', async t => {
-  const repo = 'huochunpeng/debug-npm#v1.0.0';
-
-  function resolveRepo(repo) {
-    if (repo === 'huochunpeng/debug-npm#v1.0.0') {
-      return {
-        type: 'git',
-        repo: 'git://github.com/huochunpeng/debug-npm.git',
-        commit: '361ff261e60f60a91da09bb259230303ccef8087'
-      };
-    }
-  }
-
-  const dir = await skeletonDir(repo, {_resolve: resolveRepo, _tmpFolder: 'tmp'});
-  t.truthy(fs.readdirSync(dir).includes('README.md'));
-  t.truthy(fs.readFileSync(path.join(dir, 'README.md'), 'utf8').includes('debug repo for npm'));
-
-  // use cache
-  const dir2 = await skeletonDir(repo, {_resolve: resolveRepo, _tmpFolder: 'tmp'});
-  t.is(dir2, dir);
-  t.truthy(fs.readdirSync(dir2).includes('README.md'));
-  t.truthy(fs.readFileSync(path.join(dir2, 'README.md'), 'utf8').includes('debug repo for npm'));
-
-  rmdir.sync('tmp');
-});
