c8ea05a1496405fe479683f70e8a956b95844c4f
Chunpeng Huo <huocp@me.com>
2019-04-23 09:39:45 +1000

    fix: properly cache github/bitbucket/gitlab repos

--- test/skeleton-dir.spec.js
+++ test/skeleton-dir.spec.js
@@ -30,7 +30,11 @@ test.serial('skeletonDir complains about unresolved repo', async t => {
   }));
 });
 
-test.serial('skeletonDir returns tmp folder untar git repo, and caches it', async t => {
+test.serial('skeletonDir complains about non-existing repo', async t => {
+  await t.throwsAsync(async() => skeletonDir('huochunpeng/not-exist'));
+});
+
+test.serial('skeletonDir returns tmp folder untar github repo, and caches it', async t => {
   mockfs({
     'tmp/.keep': ''
   });
@@ -47,6 +51,38 @@ test.serial('skeletonDir returns tmp folder untar git repo, and caches it', asyn
   t.truthy(fs.readFileSync(path.join(dir2, 'README.md'), 'utf8').includes('debug repo for npm'));
 });
 
-test.serial('skeletonDir complains about non-existing repo', async t => {
-  await t.throwsAsync(async() => skeletonDir('huochunpeng/not-exist'));
+test.serial('skeletonDir returns tmp folder untar bitbucket repo, and caches it', async t => {
+  mockfs({
+    'tmp/.keep': ''
+  });
+
+  const repo = 'bitbucket:huochunpeng/debug-npm#v1.0.0';
+
+  const dir = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  t.truthy(fs.readdirSync(dir).includes('README.md'));
+  t.truthy(fs.readFileSync(path.join(dir, 'README.md'), 'utf8').includes('debug repo for npm'));
+
+  const dir2 = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  t.is(dir2, dir);
+  t.truthy(fs.readdirSync(dir2).includes('README.md'));
+  t.truthy(fs.readFileSync(path.join(dir2, 'README.md'), 'utf8').includes('debug repo for npm'));
 });
+
+test.serial('skeletonDir returns tmp folder untar gitlab repo, and caches it', async t => {
+  mockfs({
+    'tmp/.keep': ''
+  });
+
+  const repo = 'gitlab:huochunpeng/debug-npm';
+
+  const dir = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  t.truthy(fs.readdirSync(dir).includes('README.md'));
+  t.truthy(fs.readFileSync(path.join(dir, 'README.md'), 'utf8').includes('debug repo for npm'));
+
+  const dir2 = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  t.is(dir2, dir);
+  t.truthy(fs.readdirSync(dir2).includes('README.md'));
+  t.truthy(fs.readFileSync(path.join(dir2, 'README.md'), 'utf8').includes('debug repo for npm'));
+});
+
+
