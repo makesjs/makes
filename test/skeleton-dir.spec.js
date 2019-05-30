f1f7538db9eae831bbdd730efd062c01b68bebc4
Chunpeng Huo <huocp@me.com>
2019-05-31 07:35:37 +1000

    refactor: replace del with lightweight rimraf

--- test/skeleton-dir.spec.js
+++ test/skeleton-dir.spec.js
@@ -4,6 +4,10 @@ import mockfs from 'mock-fs';
 import path from 'path';
 import fs from 'fs';
 
+function _useGitRepo() {
+  throw new Error('Not implemented');
+}
+
 test.afterEach(() => {
   mockfs.restore();
 });
@@ -14,26 +18,26 @@ test.serial('skeletonDir simply returns local dir', async t => {
     './my/dir/.keep': '',
     '/my/dir/.keep': ''
   });
-  t.is(await skeletonDir('../my/dir'), '../my/dir');
-  t.is(await skeletonDir('./my/dir'), './my/dir');
-  t.is(await skeletonDir('/my/dir'), '/my/dir');
+  t.is(await skeletonDir('../my/dir', {_useGitRepo}), '../my/dir');
+  t.is(await skeletonDir('./my/dir', {_useGitRepo}), './my/dir');
+  t.is(await skeletonDir('/my/dir', {_useGitRepo}), '/my/dir');
 });
 
 test.serial('skeletonDir complains about missing local dir', async t => {
   mockfs();
-  await t.throwsAsync(async() => skeletonDir('./my/dir'));
+  await t.throwsAsync(async() => skeletonDir('./my/dir', {_useGitRepo}));
 });
 
 test.serial('skeletonDir complains about unresolved repo', async t => {
   function resolveRepo() {}
 
   await t.throwsAsync(async() => skeletonDir('something', {
-    _resolve: resolveRepo
+    _resolve: resolveRepo, _useGitRepo
   }));
 });
 
 test.serial('skeletonDir complains about non-existing repo', async t => {
-  await t.throwsAsync(async() => skeletonDir('3cp/not-exist'));
+  await t.throwsAsync(async() => skeletonDir('3cp/not-exist', {_useGitRepo}));
 });
 
 test.serial('skeletonDir returns tmp folder untar github repo, and caches it', async t => {
@@ -43,11 +47,11 @@ test.serial('skeletonDir returns tmp folder untar github repo, and caches it', a
 
   const repo = '3cp/debug-npm#v1.0.0';
 
-  const dir = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  const dir = await skeletonDir(repo, {_tmpFolder: 'tmp', _useGitRepo});
   t.truthy(fs.readdirSync(dir).includes('README.md'));
   t.truthy(fs.readFileSync(path.join(dir, 'README.md'), 'utf8').includes('debug repo for npm'));
 
-  const dir2 = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  const dir2 = await skeletonDir(repo, {_tmpFolder: 'tmp', _useGitRepo});
   t.is(dir2, dir);
   t.truthy(fs.readdirSync(dir2).includes('README.md'));
   t.truthy(fs.readFileSync(path.join(dir2, 'README.md'), 'utf8').includes('debug repo for npm'));
@@ -60,11 +64,11 @@ test.serial('skeletonDir returns tmp folder untar bitbucket repo, and caches it'
 
   const repo = 'bitbucket:huochunpeng/debug-npm#v1.0.0';
 
-  const dir = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  const dir = await skeletonDir(repo, {_tmpFolder: 'tmp', _useGitRepo});
   t.truthy(fs.readdirSync(dir).includes('README.md'));
   t.truthy(fs.readFileSync(path.join(dir, 'README.md'), 'utf8').includes('debug repo for npm'));
 
-  const dir2 = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  const dir2 = await skeletonDir(repo, {_tmpFolder: 'tmp', _useGitRepo});
   // bitbucket etag is not always consistent.
   // t.is(dir2, dir);
   t.truthy(fs.readdirSync(dir2).includes('README.md'));
@@ -78,11 +82,11 @@ test.serial('skeletonDir returns tmp folder untar gitlab repo, and caches it', a
 
   const repo = 'gitlab:huochunpeng/debug-npm';
 
-  const dir = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  const dir = await skeletonDir(repo, {_tmpFolder: 'tmp', _useGitRepo});
   t.truthy(fs.readdirSync(dir).includes('README.md'));
   t.truthy(fs.readFileSync(path.join(dir, 'README.md'), 'utf8').includes('debug repo for npm'));
 
-  const dir2 = await skeletonDir(repo, {_tmpFolder: 'tmp'});
+  const dir2 = await skeletonDir(repo, {_tmpFolder: 'tmp', _useGitRepo});
   // gitlab etag is not always consistent.
   // t.is(dir2, dir);
   t.truthy(fs.readdirSync(dir2).includes('README.md'));
