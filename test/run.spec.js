a65093242878f33594bfd8f95e70feac179d749a
Chunpeng Huo <huocp@me.com>
2022-06-09 16:47:48 +1000

    test: cleanup win32 test

--- test/run.spec.js
+++ test/run.spec.js
@@ -1,29 +1,32 @@
 const test = require('ava');
 const run = require('../lib/run');
 
+const ls = process.platform === 'win32' ? 'dir' : 'ls';
+const cmdThatWait = process.platform === 'win32' ? 'cmd' : 'cat';
+
 test('run a command', async t => {
   await run('echo', ['hello']);
   t.pass('successfully run a command');
 });
 
 test('run a command in a different cwd', async t => {
-  await run('ls', ['index.js'], 'lib');
+  await run(ls, ['index.js'], 'lib');
   t.pass('successfully run a command in a different cwd');
 });
 
 test('run a command, catch failure', async t => {
-  await t.throwsAsync(async () => run('ls', ['no-such-file'], 'lib'));
+  await t.throwsAsync(async () => run(ls, ['no-such-file'], 'lib'));
 });
 
 test.cb('run a command, catch signal on exit', t => {
-  const cmd = run('cat');
+  const cmd = run(cmdThatWait);
   cmd.then(
     () => {
       t.fail('Should not pass');
       t.end();
     },
     err => {
-      t.is(err.message, 'cat exit signal: SIGTERM');
+      t.is(err.message, cmdThatWait + ' exit signal: SIGTERM');
       t.end();
     }
   );
