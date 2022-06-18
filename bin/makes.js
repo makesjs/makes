16dcf69ebfee46398280e978ec0fe0877d86b88f
Chunpeng Huo <huocp@me.com>
2022-06-18 19:48:02 +1000

    chore: be friendly with Nodejs v12

--- bin/makes.js
+++ bin/makes.js
@@ -5,35 +5,37 @@ const meta = fs.readFileSync(new URL('../package.json', import.meta.url), 'utf8'
 const { version } = JSON.parse(meta);
 const help = fs.readFileSync(new URL('./help.txt', import.meta.url), 'utf8');
 
-let makes;
+let getMakes;
 if (process.env.MAKES_RUN_SRC) {
   // only for local source debug
-  makes = (await import('../lib/index.js'));
+  getMakes = import('../lib/index.js');
 } else {
   // for production
-  makes = (await import('../dist/index.js'));
+  getMakes = import('../dist/index.js');
 }
 
-console.log(`makes v${version} https://makes.js.org`);
+getMakes.then(makes => {
+  console.log(`makes v${version} https://makes.js.org`);
 
-const {
-  supplier,
-  predefinedProperties,
-  preselectedFeatures,
-  unattended,
-  here
-} = makes.getOpts(process.argv.slice(2), help);
+  const {
+    supplier,
+    predefinedProperties,
+    preselectedFeatures,
+    unattended,
+    here
+  } = makes.getOpts(process.argv.slice(2), help);
 
-makes.default(supplier, {
-  predefinedProperties,
-  preselectedFeatures,
-  unattended,
-  here
-}).catch(e => {
-  if (e.name === 'SoftError') {
-    console.error(e.message);
-  } else {
-    console.error(e);
-  }
-  process.exit(1);
+  makes.default(supplier, {
+    predefinedProperties,
+    preselectedFeatures,
+    unattended,
+    here
+  }).catch(e => {
+    if (e.name === 'SoftError') {
+      console.error(e.message);
+    } else {
+      console.error(e);
+    }
+    process.exit(1);
+  });
 });
